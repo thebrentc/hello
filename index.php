7c810675d4333d571f7b5a6ec4b686933ecc8268
thebrentc <thebrentc@gmail.com>
2015-05-24 11:45:20 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html/>
+<html>
+<head><title>Hello</title>
+</head>
+<h1>Hello</h1>
+<?php
+echo '<p>'.$_SERVER['REMOTE_ADDR'].'</p>';
+echo '<p>'.$_SERVER['HTTP_USER_AGENT'].'</p>';
+?>
+<body> 
+</body>
+</html>
+
